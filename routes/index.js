fce5a53a1e13eb5383c0f8485785ab89dc1f5e48
Becka <rebecca.lelew@gmail.com>
2023-10-15 16:37:09 +0100

    Update index title to check automatic deploys

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Becka\'s app' });
+  res.render('index', { title: 'Becka\'s very cool app' });
 });
 
 module.exports = router;
